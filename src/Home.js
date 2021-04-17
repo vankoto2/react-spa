a43fdb58ae4827890d98da6c4e1668b00df1083c
vankoto2 <it.work.bulgaria@gmail.com>
2021-04-17 16:16:20 +0300

    Outputting lists
    Home blocs

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', autor: 'Mario', id: 1},
+        { title: 'My new blog', body: 'lorem ipsum...', autor: 'Mario', id: 2},
+        { title: 'My new test', body: 'lorem ipsum...', autor: 'Mario', id: 3},
+    ]);
+
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Writen by {blog.autor}</p>
+                </div>
+            ))}
         </div>
     );
 }
