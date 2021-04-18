3322c109d8616279a98950f02425f8fa3941acf8
vankoto2 <it.work.bulgaria@gmail.com>
2021-04-18 19:29:22 +0300

    Starting work on Create

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,23 @@ const Create = () => {
     return ( 
         <div className="create">
             <h2>Add a new Blog</h2>
+            <form>
+                <label>Blog titel:</label>
+                <input
+                type="text"
+                required
+                />
+                 <label>Blog body:</label>
+                <textarea required
+                ></textarea>
+                <label>Blog author:</label>
+                <select>
+                    <option value="JavaScript">JavaScript</option>
+                    <option value="TypeScript">TypeScript</option>
+                    <option value="Java">Java</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
      );
 }
