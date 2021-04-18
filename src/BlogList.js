d982314a9cc8990bf3b115b7f5e01cf41d256ff0
vankoto2 <it.work.bulgaria@gmail.com>
2021-04-18 15:34:18 +0300

    create db and fetch
    Created database, fetch request and a installed  JSON server

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,9 +1,10 @@
-const BlogList = ({ blogs }) => {
+const BlogList = ({ blogs, handleDelete }) => {
 
 
 
     return (
         <div>
+            <h2>All Blogs</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', autor: 'Mario', id: 1},
-        { title: 'My new blog', body: 'lorem ipsum...', autor: 'Mario', id: 2},
-        { title: 'My new test', body: 'lorem ipsum...', autor: 'Mario', id: 3},
-    ]);
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                if (!res.ok) {
+                    throw Error('coud not fetch datafrom that resource');
+                }
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setIsPending(false);
+                setError(err.message);
+            })
+    }, []);
 
     return (
         <div className="home">
-            <h2>All Blogs</h2>
-            <BlogList blogs={blogs} />
+            { error && <div>{ error }</div> }
+            {isPending && <div>Loading</div>}
+            {blogs && <BlogList blogs={blogs} />}
         </div>
     );
 }
