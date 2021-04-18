1e7d6defa03add7f65e27058a11cbe3b0aaeb8b8
vankoto2 <it.work.bulgaria@gmail.com>
2021-04-18 18:41:08 +0300

    Updating BlogDetails

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,14 +1,15 @@
+import { Link } from "react-router-dom"
+
 const BlogList = ({ blogs, handleDelete }) => {
-
-
-
     return (
         <div>
             <h2>All Blogs</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
+                    <Link to={`/blog/${blog.id}`}>
                     <h2>{blog.title}</h2>
-                    <p>Writen by {blog.autor}</p>
+                    <p>Writen by {blog.author}</p>
+                    </Link>
                 </div>
             ))}
         </div>
--- src/useFetch.js
+++ src/useFetch.js
@@ -7,6 +7,7 @@ const useFetch = (url) => {
 
     useEffect(() => {
         const abortCont = new AbortController();
+        
         fetch(url, { signal: abortCont.signal })
             .then(res => {
                 if (!res.ok) {
