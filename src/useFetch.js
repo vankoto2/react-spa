178f8fa2af37bc0c09dcc6de28660bc2b68c49a5
vankoto2 <it.work.bulgaria@gmail.com>
2021-04-18 16:57:42 +0300

    Installing router and router links

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,7 +6,8 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch(url)
+        const abortCont = new AbortController();
+        fetch(url, { signal: abortCont.signal })
             .then(res => {
                 if (!res.ok) {
                     throw Error('coud not fetch datafrom that resource');
@@ -19,9 +20,13 @@ const useFetch = (url) => {
                 setError(null);
             })
             .catch(err => {
+                if (err.name === 'AbortError') {
+                } else{
                 setIsPending(false);
                 setError(err.message);
+                }
             })
+            return () => abortCont.abort();
     }, [url]);
 
     return {data, isPending, error}
